4aaabbe29e11ec1514f560f084c4fc50b8cc26cc
Sharma <Vishal.Sharma@insight.com>
2023-07-13 19:22:32 +0530

    added main file

---
+++ practice.py
@@ -0,0 +1,14 @@
+
+
+# def greet_with_name(name):
+#     print(f"Hii {name}")
+#     print(f"How are you {name}")
+
+# greet_with_name(123)
+
+
+def greet_with(name,location):
+    print(f"Hii, {name}")
+    print(f"What is it like in {location}")
+
+greet_with(name='Angela', location='Abroad')
