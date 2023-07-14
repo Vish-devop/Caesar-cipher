243308085479a77bd3243719b3b85f4c7246f290
Sharma <Vishal.Sharma@insight.com>
2023-07-14 11:14:33 +0530

    modified art and main file

--- main.py
+++ main.py
@@ -2,9 +2,43 @@
 alphabet = ['!','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-direction = input("Type 'encode' to encrypt, or type 'decode' to decrypt\n")
-text=input("Enter the text\n").lower()
-shift=int(input("Enter the shift amount\n"))
+
+#combined the encrypt() and decrypt() functions into a single function.
+def caesar(start_text,shift_amount,cipher_direction):
+    end_text=""
+    if cipher_direction=="decode":
+        shift_amount*=-1
+    for char in start_text:
+        if char in alphabet:
+            position=alphabet.index(char)
+            new_position=position+shift_amount
+            end_text+=alphabet[new_position]
+        else:
+            end_text+=char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+from art import logo
+print(logo)
+
+
+should_end=False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, or type 'decode' to decrypt\n")
+    text=input("Enter the text\n").lower()
+    shift=int(input("Enter the shift amount\n"))
+
+    shift=shift%26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart=input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
+    if restart=="no":
+        should_end=True
+        print("Goodbye")
+
+    
+
+
 
 # def encrypt(plain_text,shift_amount):
 #     cipher_text=""
@@ -31,19 +65,5 @@ shift=int(input("Enter the shift amount\n"))
 
 
 
-
-
-#combined the encrypt() and decrypt() functions into a single function.
-def caesar(start_text,shift_amount,cipher_direction):
-    end_text=""
-    if cipher_direction=="decode":
-        shift_amount+=1
-    for letter in start_text:
-        position=alphabet.index(letter)
-        new_position=position+shift_amount
-        end_text+=alphabet[new_position]
-    print(f"Here's the {direction}d result: {end_text}")
-
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
 
